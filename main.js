15e9dc4fa19c69ae7f8d048d9fd411acf6eef540
Reynaldo <reycastano97@gmail.com>
2023-02-19 23:00:04 -0600

    Añadiendo parte final del proyecto

---
+++ main.js
@@ -0,0 +1,7 @@
+function datosFormulario() {
+    let nombre = document.getElementById("FName").value;
+    let correoElectronico = document.getElementById("YourEmail").value;
+}
+
+let datosFormulaioJSON = JSON.stringify(datosFormulario); // Asignando los datos de formulario al archivo JSON
+
